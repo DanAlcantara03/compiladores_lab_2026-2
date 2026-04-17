eb8e7300a763a30b1fadecf08e75aca9f0a66888
Daniel Alcantara Andrade <DanielAlcantara@ciencias.unam.mx>
2026-04-16 23:05:59 -0600

    feat(tools/lalr_trace_driver.cpp): Add the principal libraries I'll use to implement the driver.

---
+++ p5-lalr_1/tools/lalr_trace_driver.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <cctype>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+namespace
+{
+
+using json = nlohmann::json;
+
+}
